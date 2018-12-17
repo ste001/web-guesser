62e835feb86fdcfaef2072468e917fbd31942b46
ste001 <asso99@hotmail.com>
2018-12-17 22:46:15 +0100

    Add limited number of guesses

--- lib/web_guesser.rb
+++ lib/web_guesser.rb
@@ -4,7 +4,10 @@ require 'sinatra/reloader'
 set :root, '../'
 set :random_number, rand(100)
 
+@@remaining_guesses = 5
+
 def feedback random, guess
+  return "You lost! A new number has been generated." if (@@remaining_guesses == 0)
   return "Way too high!" if (guess > random + 5)
   return "Way too low!" if (guess < random - 5)
   return "Too high!" if (guess > random)
@@ -13,6 +16,12 @@ def feedback random, guess
 end
 
 get '/' do
+  @@remaining_guesses -= 1
   message = feedback(settings.random_number, params["guess"].to_i)
-  erb :index, :locals => {:random_number => settings.random_number, :message => message}
+  if (@@remaining_guesses == 0 || message == "You got it right!")
+    settings.random_number = rand(100)
+    @@remaining_guesses = 5
+  end
+  erb :index, :locals => {:random_number => settings.random_number,\
+     :message => message, :remaining_guesses => @@remaining_guesses}
 end
