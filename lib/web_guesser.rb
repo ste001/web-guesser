8815ef67afa59d8d90e10a26c4b80319c53e641f
ste001 <asso99@hotmail.com>
2018-12-17 12:25:00 +0100

    Complete number guessing game

--- lib/web_guesser.rb
+++ lib/web_guesser.rb
@@ -5,6 +5,15 @@ set :root, '../'
 
 random_number = rand(100)
 
+def feedback random, guess
+  return "Way too high!" if (guess > random + 5)
+  return "Way too low!" if (guess < random - 5)
+  return "Too high!" if (guess > random)
+  return "Too low!" if (guess < random)
+  "You got it right!"
+end
+
 get '/' do
-  erb :index, :locals => {:random_number => random_number}
+  message = feedback(random_number, params["guess"].to_i)
+  erb :index, :locals => {:random_number => random_number, :message => message}
 end
